4ae7f63f22323d99544a22e1e68d012f85ccc275
Andrew Martinek <andrewdmartinek@gmail.com>
2022-03-24 07:41:09 -0500

    put in html inputs with css spacing, reset js file

--- script.js
+++ script.js
@@ -1,11 +1,5 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var char = "abcdefghijklmnopqrstuvwxyz"
-var sym = "!@#$%^&*'-_=+|;:?.,`~<>/"
-var num = "0123456789"
-
-// add checkbox for number y/n
-// add checkbox for symbol y/n
 
 // Write password to the #password input
 function writePassword() {
